bbb6225dc15f7144bee2d81dbfa1e5e2b0069dc3
Mrit Asid <ww771282337@gmail.com>
2024-08-14 02:08:45 +0300

    تمت اضافة حل التحدي للفديو رقم30

---
+++ Video_solution_No_30/chelig30.js
@@ -0,0 +1,25 @@
+let a = "Elzero Web School"
+
+// =================1=================
+console.log(a.charAt(2).toLocaleUpperCase() + a.substring(3,6))
+
+ 
+// =================2=================
+
+console.log(a.charAt(13).toUpperCase().repeat(8))
+
+// =================3=================
+
+console.log(a.split(" ", 1))
+
+
+// =================4=================
+
+console.log(a.substring(0, 6) + a.substring(10, ))
+
+// =================5=================
+
+console.log(a.charAt(0).toLocaleLowerCase()
+             +  a.substring(1, a.length - 1).toLocaleUpperCase() 
+             + a.substr(-1))
+
